52868931bc3691496d26129f8943d82b21ec37c5
Daniel <daniel10gja@gmail.com>
2024-08-20 01:10:48 -0300

    Create annotations

---
+++ AppDeVendas/src/main/java/annotation/TipoChave.java
@@ -0,0 +1,11 @@
+package annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TipoChave {
+
+    String value();
+}
