52e1acbae12eb6498b677b0bdd8eb023b6d6b783
Daniel <daniel10gja@gmail.com>
2024-08-20 01:24:02 -0300

    Create AppDeVendasApplication.java

---
+++ AppDeVendas/src/main/java/br/com/danielsantdev/appdevendas/AppDeVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielsantdev.appdevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDeVendasApplication.class, args);
+    }
+
+}
