fa3ec2a7a024c3709295d2e1564dfb75781ec778
Daniel <daniel10gja@gmail.com>
2024-08-20 01:09:51 -0300

    Create dao package Classes

---
+++ AppDeVendas/src/main/java/br/com/danielsantdev/dao/IProdutoDAO.java
@@ -0,0 +1,8 @@
+package br.com.danielsantdev.dao;
+
+import br.com.danielsantdev.dao.generic.IGenericDAO;
+import br.com.danielsantdev.domain.Produto;
+
+public interface IProdutoDAO extends IGenericDAO<Produto, String>{
+
+}
